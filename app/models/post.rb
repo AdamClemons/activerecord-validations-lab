6aa244ff0d33ab4bde48dbe9488811b14e236aaf
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2022-02-14 17:53:59 -0500

    commit tree 5e066965789e6bd298c8f227107b7d24c8c55faa

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    validate :clickbait
+
+    def clickbait
+        needs = ["Won't Believe", "Secret", "Top "]
+
+        is_bait = needs.any? do |bait|
+            self.title.include?(bait) if self.title
+        end
+
+        errors.add(:title, "Is not Clickbait") if !is_bait
+
+    end
 end
